892e2af5f66d6f13b8251a791165ef471ae4dc3f
riky moroni <riky.moroni5@gmail.com>
2022-04-06 22:59:50 +0100

    add die component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import './App.css';
+import Die from './components/Die'
 
 function App() {
   return (
     <main className="main">
-     
+      <div className="wrapper">
+        <Die
+        value={1}
+        />
+        <Die
+        value={6}
+        />
+        <Die
+        value={3}
+        />
+        <Die
+        value={4}
+        />
+        <Die
+        value={1}
+        />
+        <Die
+        value={4}
+        />
+        <Die
+        value={3}
+        />
+        <Die
+        value={2}
+        />
+        <Die
+        value={1}
+        />
+        <Die
+        value={3}
+        />
+      </div>
     </main>
   );
 }
---
+++ src/components/Die.js
@@ -0,0 +1,7 @@
+import './Die.css';
+
+export default function Die(props) {
+  return (
+    <div className="die">{props.value}</div>
+  )
+}
