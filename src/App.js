0edf5e3857c7f138719e48d7ecf06cfae68174cb
riky moroni <riky.moroni5@gmail.com>
2022-04-06 23:29:03 +0100

    add allNewDice function, useState to generate new Dice

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
+import { useState } from 'react';
 import './App.css';
 import Die from './components/Die'
 
 function App() {
+  function allNewDice() {
+    const newDice = []
+      for (let i = 0; i < 10; i++) {
+        newDice.push(Math.ceil(Math.random() * 6))
+      }
+      return newDice
+  }
+
+  const [dice, setDice] = useState(allNewDice)
+
+  const generateDie = dice.map((num,i)=>{
+    return (
+      <Die key={i} value={num} />
+    )
+  })
+
   return (
     <main className="main">
       <div className="wrapper">
-        <Die value={1} />
-        <Die value={6} />
-        <Die value={6} />
-        <Die value={6} />
-        <Die value={6} />
-        <Die value={6} />
-        <Die value={6} />
-        <Die value={6} />
-        <Die value={6} />
-        <Die value={6} />
+        {generateDie}
       </div>
     </main>
   );
