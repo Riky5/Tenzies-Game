2bb3cc0a1dc4af9cabbf5b50780ec912c04fdf32
riky moroni <riky.moroni5@gmail.com>
2022-04-06 23:48:08 +0100

    add roll dice button

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Die from './components/Die'
 
 function App() {
+  const [dice, setDice] = useState(allNewDice)
+
   function allNewDice() {
     const newDice = []
       for (let i = 0; i < 10; i++) {
@@ -11,19 +13,18 @@ function App() {
       return newDice
   }
 
-  const [dice, setDice] = useState(allNewDice)
+  function rollDice() {
+    setDice(allNewDice)      
+  }
 
-  const generateDie = dice.map((num,i)=>{
-    return (
-      <Die key={i} value={num} />
-    )
-  })
+  const generateDie = dice.map((num,i)=> <Die key={i} value={num} />)
 
   return (
     <main className="main">
       <div className="wrapper">
         {generateDie}
       </div>
+      <button className='roll-btn' onClick={rollDice}>Roll Dice</button>
     </main>
   );
 }
