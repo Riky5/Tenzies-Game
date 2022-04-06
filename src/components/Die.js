1e701ea11ffa0a2a41102d86d557a8cb70fde399
riky moroni <riky.moroni5@gmail.com>
2022-04-06 23:07:36 +0100

    resize number on dice

--- src/components/Die.js
+++ src/components/Die.js
@@ -2,6 +2,8 @@ import './Die.css';
 
 export default function Die(props) {
   return (
-    <div className="die">{props.value}</div>
+    <div className="die">
+      <h1>{props.value}</h1>
+    </div>
   )
 }
